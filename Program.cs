b9b3804dc33653837ee6664241e19a8fe1b48c67
MetalJames <volodyaruzhak@gmail.com>
2024-02-11 18:08:23 -0500

    Create method to update player's position

--- Program.cs
+++ Program.cs
@@ -24,6 +24,28 @@ public class Player
         Position = position;
         GemCount = 0;
     }
+
+    // Method to update player's position based on the input direction (U, D, L, R)
+    public void Move(char direction)
+    {
+        switch (direction)
+        {
+            case 'U':
+                Position = new Position(Position.X, Position.Y - 1);
+                break;
+            case 'D':
+                Position = new Position(Position.X, Position.Y + 1);
+                break;
+            case 'L':
+                Position = new Position(Position.X - 1, Position.Y);
+                break;
+            case 'R':
+                Position = new Position(Position.X + 1, Position.Y);
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 public class Game
