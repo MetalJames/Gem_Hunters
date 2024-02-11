ccde8e9e726b45d62d919c7b891ca51232d6b05c
MetalJames <volodyaruzhak@gmail.com>
2024-02-11 17:39:22 -0500

    Create Position Class and partially create Player class without method

--- Program.cs
+++ Program.cs
@@ -3,9 +3,34 @@
     public int X { get; }
     public int Y { get; }
 
+    // Position Constructor
     public Position(int x, int y)
     {
         X = x;
         Y = y;
     }
 }
+
+public class Player
+{
+    public string Name { get; }
+    public Position Position { get; private set; }
+    public int GemCount { get; private set; }
+
+    // Player Constructor
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+    }
+}
+
+public class Game
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new Player("John", new Position(3, 4)));
+    }
+}
+
